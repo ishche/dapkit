fb7c882f379452541315b643d1f78e493dd84131
Iurii Shchekochikhin <iurii.shchekochikhin@broadcom.com>
2020-07-03 16:27:12 +0200

    #9 add commang WIP

--- vscode/x0a-dapkit/src/extension.ts
+++ vscode/x0a-dapkit/src/extension.ts
@@ -1,6 +1,8 @@
 import * as vscode from 'vscode';
+import { createWithPapProxyLaunchConfiguration } from "./commands";
 
 export function activate(context: vscode.ExtensionContext) {
+    vscode.commands.registerCommand("x0a-dapkit.create-with-dap-proxy-launch-configuration", createWithPapProxyLaunchConfiguration);
 }
 
-export function deactivate() {}
+export function deactivate() { }
